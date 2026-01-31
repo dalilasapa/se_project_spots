86bac8e22ebbea1217f2641242ebbd9b5ecdbfbd
dalilasapa <dalilasapa@Dalilas-MacBook-Air.local>
2026-01-30 22:11:15 -0600

    Sprint 4 Final Stages

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,24 @@ const editProfileCloseBtn = editProfileModal.querySelector(
 const newPostBtn = document.querySelector(".profile__post-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescElement = document.querySelector(".profile__description");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const nameInput = newPostModal.querySelector("#new-caption-input");
+const linkInput = newPostModal.querySelector("#link-url-input");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescInput.value = profileDescElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 editProfileCloseBtn.addEventListener("click", function () {
@@ -21,3 +37,20 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescElement.textContent = editProfileDescInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+  console.log("submitting");
+}
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  newPostModal.classList.remove("modal_is-opened");
+  console.log(linkInput.value);
+  console.log(nameInput.value);
+}
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
